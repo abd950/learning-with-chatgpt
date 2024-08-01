2e1fd1fd0016584942b13d9c2cd06a954f061541
abd950 <jahabdulrahman1000@gmail.com>
2024-08-01 16:37:21 -0400

    added changes

--- debug1/script.js
+++ debug1/script.js
@@ -1,20 +1,20 @@
+    let xPos = 250;
+    let yPos = 250;
+    let xSpeed = 1;
+    let ySpeed = 5;
+
 function setup() {
     createCanvas(500, 500);
     background(0);
-    nostroke();
-
-    let xPos = 0;
-    let yPos = 0;
-    let xSpeed = 1;
-    let ySpeed = 4.5;
-
+    noStroke();
+}
 
 function draw() {
-    background(0, 0, 0, 5);
+    background(0, 0, 0, 10);
 
     fill(255, 0, 255);
     ellipse(xPos, yPos, 30, 30);
-
+    
     xPos += xSpeed;
     yPos += ySpeed;
 
@@ -24,5 +24,4 @@ function draw() {
     if (yPos < 15 || yPos > 485) {
         ySpeed *= -1;
     }
-}
 }
